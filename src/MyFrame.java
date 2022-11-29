38378c56713d1b2eb7be41c139ba6e850177500e
DK <dongwook5478@gmail.com>
2022-11-29 13:48:56 +0900

    empty

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,61 +1,6 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
-
-public class MyFrame extends JFrame {
-    //private JLabel color=new JLabel();
-    private JTextField txt=new JTextField(10);
-    private JTextArea area=new JTextArea(10,10);
-    public MyFrame(){
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //JTextArea area=new JTextArea(10,10);
-        Container c=getContentPane();
-
-        c.setLayout(new FlowLayout());
-        c.add(txt);
-        c.add(area);
-
-        txt.addKeyListener(new MyKeyListener());
-        c.setFocusable(true);
-        c.requestFocus();
-        //c.add(color);
-
-        setSize(200,300);
-        setVisible(true);
-
-    }
-class MyKeyListener extends KeyAdapter{
-
-    public void keyReleased(KeyEvent e){
-
-        int r=(int) (Math.random()*256);
-        int g=(int) (Math.random()*256);
-        int b=(int) (Math.random()*256);
-
-        if (e.getKeyChar() == KeyEvent.VK_0 ||
-                e.getKeyCode() == KeyEvent.VK_1 || e.getKeyCode() == KeyEvent.VK_2 ||
-                e.getKeyCode() == KeyEvent.VK_3 || e.getKeyCode() == KeyEvent.VK_4 ||
-                e.getKeyCode() == KeyEvent.VK_5 || e.getKeyCode() == KeyEvent.VK_6 ||
-                e.getKeyCode() == KeyEvent.VK_7 || e.getKeyCode() == KeyEvent.VK_8 ||
-                e.getKeyCode() == KeyEvent.VK_9){
-
-
-            txt.setForeground(new Color(r,g,b));
-            txt.setFont(new Font("Bold",Font.BOLD,20));
-            
-        } else if (e.getKeyChar()== KeyEvent.VK_ENTER) {
-            area.append(txt.getText()+"\n");
-            txt.setText("");
-        }
-
-
-    }
-}
+public class MyFrame {
     public static void main(String[] args){
-        new MyFrame();
 
     }
 }
