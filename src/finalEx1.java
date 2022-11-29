215cb8551bca9b8a808894139a4cb9ed9d7fbe3c
DK <dongwook5478@gmail.com>
2022-11-29 16:39:42 +0900

    minor changes

--- src/finalEx1.java
+++ src/finalEx1.java
@@ -13,14 +13,11 @@ public class finalEx1 {
         Vector<Integer> ovalue=new Vector<Integer>();
         Set<String> keys=w.keySet();
 
-
         boolean tf=true;
         System.out.println("첫번째 HASHMAP 데이터를 입력하세요 >>>");
 
         while(tf) {
-
             String a = sc.next();
-
             if (a.equals("quit")) {
                 tf = false;
                 System.out.println("1) 출력 결과");
@@ -45,7 +42,6 @@ public class finalEx1 {
         HashMap<String, Double> h=new HashMap<String, Double>();
         Set<String> keys2=h.keySet();
 
-
         boolean tf2=true;
         System.out.println("두번째 HASHMAP 데이터를 입력하세요 >>>");
 
@@ -69,9 +65,7 @@ public class finalEx1 {
             h.put(aa, bb);
         }
 
-
 //2-3
-
         System.out.println("번호\t몸무게\t키");
         for (int i = 0; i< okeys.size();i++){
             Iterator<String> it=keys2.iterator();
@@ -80,11 +74,7 @@ public class finalEx1 {
                 if (okeys.get(i).equals(item)){
                     System.out.println(item+"\t"+ovalue.get(i)+"\t\t"+h.get(item) );
                 }
-
             }
         }
-
-
-
     }
 }
